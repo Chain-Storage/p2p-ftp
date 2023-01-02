a1b3371407fae385fdbee2fb642ee71e8b53c054
yurikaza <yusufmirza55@hotmail.com>
2023-01-02 14:48:57 +0300

    getFiles doneeeeeee!!

--- src/ftp/getFiles.ts
+++ src/ftp/getFiles.ts
@@ -1,22 +1,35 @@
 import fs from "fs";
 
-async function getFiles(fileHash: string) {
+export async function getFiles(
+  fileHash: string,
+  fileName: string
+): Promise<void> {
   let fileData: any[] = [];
 
   for (let index: number = 0; index < 6; index++) {
-    fs.readFile(
+    const data: any = fs.readFileSync(
       __dirname + `/files/${fileHash}-${index}`,
       {
         encoding: "utf-8",
-      },
-      (err, data) => {
-        console.log(data);
-        fileData.push(data);
+        flag: "r",
       }
     );
+
+    const buff = Buffer.from(data, "utf-8");
+    console.log(buff);
+
+    fileData.push(buff);
   }
 
   console.log(fileData);
+
+  const newBuffer = Buffer.concat(fileData);
+  console.log(newBuffer);
+
+  fs.writeFileSync(`${__dirname}/files/${fileName}`, newBuffer);
 }
 
-getFiles("Ecb63dfc5f50b4e447d706a3e549df2ef15228cc80189b976b475a2beeb84904bb");
+getFiles(
+  "Ecb63dfc5f50b4e447d706a3e549df2ef15228cc80189b976b475a2beeb84904bb",
+  ".gitignore"
+);
